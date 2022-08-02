555c19ebf81fcac7d9bd25ef7a00e29f58749be4
SantiagoVira <8bitharrypotter@gmail.com>
2022-08-02 18:05:31 -0400

    fix temporary fix

--- src/hooks/useErrorCode.ts
+++ src/hooks/useErrorCode.ts
@@ -43,23 +43,21 @@ export const useCheckError = () => {
   );
 
   return useCallback(() => {
-    return () => {
-      // if (originalImageFile === "") setErrorCode(1);
-      // else if (gridRows === "" || parseInt(gridRows) < 1) setErrorCode(2);
-      // else if (gridCols === "" || parseInt(gridCols) < 1) setErrorCode(3);
-      // else if (parseInt(gridRows) > 50) setErrorCode(4);
-      // else if (parseInt(gridCols) > 50) setErrorCode(5);
-      // else if (stuff.current.width < 300 || stuff.current.height < 300)
-      //   setErrorCode(6);
-      // else setErrorCode(0);
-      // console.log(
-      //   "useCallback",
-      //   stuff,
-      //   stuff.current,
-      //   stuff.current.width,
-      //   stuff.current.height
-      // );
-      setErrorCode(0);
-    };
+    // if (originalImageFile === "") setErrorCode(1);
+    // else if (gridRows === "" || parseInt(gridRows) < 1) setErrorCode(2);
+    // else if (gridCols === "" || parseInt(gridCols) < 1) setErrorCode(3);
+    // else if (parseInt(gridRows) > 50) setErrorCode(4);
+    // else if (parseInt(gridCols) > 50) setErrorCode(5);
+    // else if (stuff.current.width < 300 || stuff.current.height < 300)
+    //   setErrorCode(6);
+    // else setErrorCode(0);
+    // console.log(
+    //   "useCallback",
+    //   stuff,
+    //   stuff.current,
+    //   stuff.current.width,
+    //   stuff.current.height
+    // );
+    setErrorCode(0);
   }, [setErrorCode]);
 };
