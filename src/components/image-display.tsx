ebe90aa50a53f9b302f3e5e6adc05e4ccd938f3f
SantiagoVira <8bitharrypotter@gmail.com>
2022-08-03 00:21:29 -0400

    Actually improve it this time

--- src/components/image-display.tsx
+++ src/components/image-display.tsx
@@ -1,4 +1,4 @@
-import { Tooltip } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 import { useAtom } from "jotai";
 import { displayImageBlobAtom } from "../atoms";
 import { useIsIOS } from "../hooks/useIsIOS";
@@ -8,30 +8,25 @@ const ImageDisplay: React.FC = () => {
   const [displayImageBlob] = useAtom(displayImageBlobAtom);
   const isIOS = useIsIOS();
 
-  return isIOS ? (
-    <Tooltip
-      label='To save your result, hold down on the image above and select "Add to Photos"'
-      borderColor="bg.500 !important"
-      border="1px solid"
-      bg="bg.900"
-      borderRadius="8px"
-      p="0.6rem"
-      placement="bottom"
-      closeDelay={200}
-      shouldWrapChildren
-      defaultIsOpen>
+  return (
+    <>
       <StyledImage
         src={displayImageBlob}
         alt="cut-up-image"
         borderRadius="16px"
       />
-    </Tooltip>
-  ) : (
-    <StyledImage
-      src={displayImageBlob}
-      alt="cut-up-image"
-      borderRadius="16px"
-    />
+      {true && (
+        <Text
+          borderColor="bg.500 !important"
+          border="1px solid"
+          bg="bg.900"
+          borderRadius="8px"
+          p="0.6rem">
+          To save your result, hold down on the image above and select "Add to
+          Photos"
+        </Text>
+      )}
+    </>
   );
 };
 
